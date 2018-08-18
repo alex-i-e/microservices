cd261f532cbfde342aff464d8eb7f790558b8609
Aleksandr Eremin <aleksandr.i.eremin@yandex.ru>
2018-08-18 20:48:28 +0300

    init

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 # Microservices
 
 script run with `nodemon`:
-
 - npm run nodmn-next
--- index.js
+++ index.js
@@ -2,7 +2,4 @@
 const app = require('express')(require('http').createServer());
 const port = process.env.PORT || 8080;
 
-
-
-
 app.listen(port, () => console.log(`App listening on port ${port}!`));
